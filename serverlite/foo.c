0975afeabfe9510372e25650d1379da11f6a4765
Sami Alameddine <sala0599@stu.gusd.net>
2020-09-28 06:57:31 +0000

    updated foo.c

--- serverlite/foo.c
+++ serverlite/foo.c
@@ -1,89 +1,57 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <arpa/inet.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include "common.h"
+#include <strings.h>
 
-char getpage[] (){
-  FILE *fp;
-  char buff[1000000];
+
+char getpage () {
+  FILE * fp;
   fp = fopen("html/main.html", "r");
-  fscanf(fp, "%s", buff);
-  return buff;
+  char sng[500];
+  while (!feof(fp)) {
+    fgets(sng, 500, fp);
+  }
 }
 
-char webpage[] = getpage();
+char wpx = getpage();
 
-int main(int arc, char argv[]) {
-  struct sockaddr_in server_addr, client_addr;
-  socklen+t sin_len = sizeof(client_addr)
-  int fd_server, fd_client;
-  char buf[4096];
-  int fdimg;
+int main(int argc, char **argv) {
+  int           listenfd, connfd, n;
+  struct sockaddr_in  serveraddr;
+  uint8_t         buff[MAXLINE + 1];
+  uint8_t         recvline[MAXLINE + 1];
 
-  fd_server = socket(AF)INET, SOCK_STREAM, 0)
-    if (fd_server < 0) {perror("socket"); exit(1);}
+  if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    err_n_die("socket error.");
+  bzero(&serveraddr, sizeof(serveraddr));
+  serveraddr.sin_family   = AF_INET;
+  serveraddr.sin_addr.s_addr  = htonl(INADDR_ANY);
+  serveraddr.sin_port     = htons(SERVER_PORT);
 
-    sotsockpot(fd_server, SOL_SOCKET, SO_REUSEADDR ,&on, sizeof(int));
+  if ((bind(listenfd, (SA *) &serveraddr, sizeof(serveraddr))) <0)
+    err_n_die("bind error.");
+  if ((listen(listenfd, 10)) < 0)
+    err_n_die("listen error.");
+  for ( ; ; ) {
+    struct sockaddr_in addr;
+    socklen_t addr_len;
 
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(8080);
+    printf("waiting for connection on port %d\n", SERVER_PORT);
+    fflush(stdout);
+    connfd = accept(listenfd, (SA *) NULL, NULL);
 
-    if(bind(fd_server, (struct sockaddr*) &server_addr, sizeof(server_addr)) == -1)
-    {
-      perror("bind");
-      close(fd_server);
-      exit(1);
+    memset(recvline, 0, MAXLINE);
+
+    while ( (n=read(connfd, recvline, MAXLINE-1) ) > 0) {
+      fprintf(stdout, "\n%s\n\n%s", bin2hex(recvline, n), recvline);
+
+      if (recvline[n-1] == '\n') {
+        break;
+      }
+
+      memset(recvline, 0, MAXLINE);
     }
-
-  if(listen(fd_server, 50) == -1) {
-    perror("listen");
-    close(fd_server);
-    exit(1);
+    if (n < 0)
+      err_n_die("read error");
+    
+    snprintf((char*)buff, sizeof(buff), "HTTP/1.0 200 OK\r\n\r\n%s",wpx);
   }
-
-  while(1) {
-
-    fd_client = accept(fd_server, (struct sockaddr *) &client_addr, &sin_len);
-
-    if(fd_client == -1) {
-      perror("Connection Failed....\n"); 
-      continue;
-    }
-
-    printf("Got Client connection...\n");
-
-    if(!fork()) {
-      /*child process*/
-      close(fd_server);
-    }
-    /*parent process*/
-    close(fd_client);
-    memset(buf, 0, 4092);
-    read(fd_client, buf, 2047);
-
-    printf("%s\n", buf);
-
-    if(!strncmp(buf, "GET /favicon.ico", 16)) {
-      fdimg = open("favicon.ico", 0_RDONLY);
-      sendfile(fd_client, fdimg, NULL, 4000);
-      close(fdimg);
-    }
-
-    write("fd_client", webpage, sizeof(webpage) -1);
-
-
-    close(fd_client);
-    printf("closing...\n");
-    exit(0);
-
-  }
-  return 0;
 }
