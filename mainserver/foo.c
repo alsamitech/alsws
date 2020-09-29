30e909637bc3e5a323e29ec8f829c0dc430fb3d5
Sami Alameddine <sala0599@stu.gusd.net>
2020-09-29 22:18:35 +0000

    fixed really really really dumb errors in foo.c

--- mainserver/foo.c
+++ mainserver/foo.c
@@ -4,13 +4,28 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-const int portnum 1243
+const int portnum = 1243;
 
 int main(int argc, char *argv[]){
-  int sockfd;
-  struct sockaddr_in serv_addr; 
+  int sockfd, newsockfd;
+  struct sockaddr_in serv_addr, cli_addr; 
   sockfd = socket(AF_INET, SOCK_STREAM,0);
   if (sockfd < 0)  {
     printf("Can't Open Socket.\n");
+    exit(1);
   }
+  serv_addr.sin_family =  AF_INET;
+  serv_addr.sin_port = htons(portnum);
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+
+  //Bind to the socket idk too much about this so I hope this compiles sucsessfully
+  int res = bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+  if (res < 0) {printf("Can't bind socket.\n");exit(1);}
+  // listen for connection whoohoo we actually might get a vistior on this site!!!
+  listen(sockfd, 5);
+  //accept the connection ill add dos protection later
+  int clilen = sizeof(cli_addr);
+  newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, (socklen_t *)&clilen);
+  //hand off to function
+
 }
