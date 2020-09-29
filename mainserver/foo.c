a0c1dc58481cafca2424683250c9ff7bf0a86f58
Sami Alameddine <sala0599@stu.gusd.net>
2020-09-29 21:50:22 +0000

    removed file ide generated

--- mainserver/foo.c
+++ mainserver/foo.c
@@ -9,7 +9,7 @@ const int portnum 1243
 int main(int argc, char *argv[]){
   int sockfd;
   struct sockaddr_in serv_addr; 
-  sockfd = socket(AF+INET, SOCK_STREAM,0);
+  sockfd = socket(AF_INET, SOCK_STREAM,0);
   if (sockfd < 0)  {
     printf("Can't Open Socket.\n");
   }
