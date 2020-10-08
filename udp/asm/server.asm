aeaec046d263137c7753a63d7a598b413b98ea4a
Sami Alameddine <sala0599@stu.gusd.net>
2020-10-08 20:13:58 +0000

    added more elements

---
+++ udp/asm/server.asm
@@ -0,0 +1,12 @@
+; Assembly Web Server, Sami Alameddine, 2020
+
+; This web Server is part of ALSWS
+
+section .data
+	msg db 'alsws'	;defines msg as alsws
+
+section .text
+	global _start
+
+_start:
+	; To do this, we will need to get th 
